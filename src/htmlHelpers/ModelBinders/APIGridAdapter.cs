83d941d7c7ff462d35881d984213ebba218fb0a3
Jose Manuel Corral <jose.corral@tokiota.com>
2018-05-07 07:41:11 +0200

    Updated for work with VS2017

--- src/htmlHelpers/ModelBinders/APIGridAdapter.cs
+++ src/htmlHelpers/ModelBinders/APIGridAdapter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Reflection;
-using System.Web.Http;
+//using System.Web.Http;
 using System.Linq.Expressions;
 using System.Web;
 
